61541b449b1c85996b7284ef1ae338972390ed64
Jethro Perez <Jethro@madison-reed.com>
2020-04-05 11:14:07 -0700

    solving_ch_2_ex

---
+++ python_for_everybody_solutions/ch_2_ex_1.py
@@ -0,0 +1,11 @@
+## Collect Hours
+hrs = input('Enter Hours:')
+
+## Collect Rate
+rph = input('Enter Rate:')
+
+## Cast fields as foat then multiply and assing to GP
+gp = float(hrs)*float(rph)
+
+## Print Gross Pay
+print(gp)
